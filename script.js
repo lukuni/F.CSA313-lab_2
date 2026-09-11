076107fb242dbcf805a1746e361f9afc9c18cc42
Oyumaa Chimeddagva <feelingjasmine@gmail.com>
2026-09-12 00:53:18 +0800

    script.js iig zasav

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
 
-export const options = { 
-  vus: 5, 
-  duration: '30s' 
-};
-
 export default function () {
   const res = http.get('http://localhost:8000/');
   check(res, { 'status 200 байна': (r) => r.status === 200 });

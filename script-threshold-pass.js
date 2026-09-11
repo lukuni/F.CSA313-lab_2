a59621ce62da0de583478ad4d4fc1a6cc6e10870
Oyumaa Chimeddagva <feelingjasmine@gmail.com>
2026-09-12 01:21:56 +0800

    run script-threshold-pass.js and script-threshoold-fail.js and saved results

---
+++ script-threshold-pass.js
@@ -0,0 +1,17 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+  vus: 30, 
+  duration: '30s',
+  thresholds: {
+    http_req_duration: ['p(95)<4.5'], // baseline хэмжилтэд үндэслэсэн SLO
+    http_req_failed:   ['rate<0.01'],
+  },
+};
+
+export default function () {
+  const res = http.get('http://localhost:8000/');
+  check(res, { 'status 200 байна': (r) => r.status === 200 });
+  sleep(1);
+}
